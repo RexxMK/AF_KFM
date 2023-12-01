ae53f5d5c7bb6a08034c450ab6d8a5eda22f0812
Svannad <112545494+Svannad@users.noreply.github.com>
2023-12-01 12:21:34 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,35 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react"
 import Logo from "../img/kfm-logo.png"
 import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 import PersonSharpIcon from '@mui/icons-material/PersonSharp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 
+
+//SD
 export default function Header() {
+    
+    const [txtMenuDisplay, setTxtMenuDisplay] = useState('none');
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 950);
+
+    const toggleTxtMenu = () => {
+        setTxtMenuDisplay((prevDisplay) => (prevDisplay === 'none' ? 'flex' : 'none'));
+    };
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 950);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
     return (
             <header>
                 <section className="sog-wrap">
@@ -23,16 +47,21 @@ export default function Header() {
                 </section>
                 <nav>
                     <div className="header-wrap">
-                        <div className="txt-menu">
-                            <a className="menupunkt">Forside</a>
-                            <NavLink to="/" className="menupunkt">Køb Bøger</NavLink>
-                            <a className="menupunkt">Køb Moleskine</a>
-                            <a className="menupunkt">Bøger vi anbefaler</a>
-                            <a className="menupunkt icon info">
-                                <p>Information</p>
-                                <ArrowDropDownIcon/>
-                            </a>
-                            <a className="menupunkt">Eventkalender</a>
+                        <button className="burger-menu" onClick={toggleTxtMenu}>
+                            <MenuSharpIcon />
+                        </button>
+                        <div className="txt-menu" style={{ display: isMobile ? txtMenuDisplay : 'flex' }}>
+                            <div className="menu-wrap">
+                                <a className="menupunkt">Forside</a>
+                                <NavLink to="/" className="menupunkt">Køb Bøger</NavLink>
+                                <a className="menupunkt">Køb Moleskine</a>
+                                <a className="menupunkt">Bøger vi anbefaler</a>
+                                <a className="menupunkt info">
+                                    <p>Information</p>
+                                    <ArrowDropDownIcon/>
+                                </a>
+                                <a className="menupunkt">Eventkalender</a>
+                            </div>
                         </div>
                         <div className="icon-menu">
                             <NavLink to="/favoritside" className="menupunkt icon">
