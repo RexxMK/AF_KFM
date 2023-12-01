79c20ee28e00bbde7bc550cd7a78b2a3561230ba
RexxMK <rikkekodahl@live.dk>
2023-12-01 11:23:52 +0100

    Kategoriunderside in progress

--- src/pages/MustRead.jsx
+++ src/pages/MustRead.jsx
@@ -1,10 +1,24 @@
 import BogkortHent from "../components/BogkortHent";
+import KategoriUnderside from "../components/KategoriUnderside";
+import VenstreBokse from "../components/VenstreBokse";
 import Bogkort from "../components/bogkort";
 
 export default function MustRead() {
   return (
     <>
-      <BogkortHent />
+      <div className="pageContainer">
+        <div className="pageFlex">
+          <VenstreBokse />
+          <div className="katUnderside">
+            <KategoriUnderside
+              headingText={"Must Read"}
+              katText={"Månedens Must Read hos KFM"}
+              antalText={"Viser 18 produkter"}
+            />
+            <BogkortHent />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
